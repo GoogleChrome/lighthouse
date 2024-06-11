d327e029320ede2f5059e7ab8a550a5844c08203
Connor Clark <cjamcl@google.com>
2024-06-11 14:12:03 -0700

    core(tests): remove usage of computed PageDependencyGraph

--- core/test/lib/lantern/metrics/metric-test-utils.js
+++ core/test/lib/lantern/metrics/metric-test-utils.js
@@ -9,6 +9,9 @@ import * as TraceEngine from '@paulirish/trace_engine';
 import {NetworkAnalyzer} from '../../../../lib/lantern/simulator/network-analyzer.js';
 import {Simulator} from '../../../../lib/lantern/simulator/simulator.js';
 import * as TraceEngineComputationData from '../../../../lib/lantern/trace-engine-computation-data.js';
+import {polyfillDOMRect} from '../../../../lib/polyfill-dom-rect.js';
+
+polyfillDOMRect();
 
 /**
  * @param {TraceEngine.Types.TraceEvents.TraceEventData[]} traceEvents
@@ -39,4 +42,7 @@ async function getComputationDataFromFixture({trace, settings, URL}) {
   };
 }
 
-export {getComputationDataFromFixture};
+export {
+  runTraceEngine,
+  getComputationDataFromFixture,
+};
