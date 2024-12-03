973055d3dda24a52ecccecc9c59cc319e3dcd8ea
Connor Clark <cjamcl@google.com>
2024-12-03 10:12:18 -0800

    fallback

--- core/computed/network-analysis.js
+++ core/computed/network-analysis.js
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import log from 'lighthouse-logger';
+
 import * as Lantern from '../lib/lantern/lantern.js';
 import {makeComputedArtifact} from './computed-artifact.js';
 import {NetworkRecords} from './network-records.js';
@@ -18,7 +20,13 @@ class NetworkAnalysis {
     const records = await NetworkRecords.request(devtoolsLog, context);
     const analysis = Lantern.Core.NetworkAnalyzer.analyze(records);
     if (!analysis) {
-      throw new Error('Could not compute network analysis');
+      log.error('NetworkAnalysis', 'Network analysis failed due to lack of transfer data');
+      return {
+        throughput: 0,
+        rtt: Number.POSITIVE_INFINITY,
+        additionalRttByOrigin: new Map(),
+        serverResponseTimeByOrigin: new Map(),
+      };
     }
     return analysis;
   }
