3a80dd35b21b1c5bb2fe616af4c481d4573ee879
Adam Raine <ad.st.raine@gmail.com>
2025-03-18 17:10:51 -0700

    core(insights): upgrade and implement forced reflow

---
+++ core/audits/insights/use-cache-insight.js
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {UIStrings} from '@paulirish/trace_engine/models/trace/insights/UseCache.js';
+
+import {Audit} from '../audit.js';
+import * as i18n from '../../lib/i18n/i18n.js';
+import {adaptInsightToAuditProduct, makeNodeItemForNodeId} from './insight-audit.js';
+
+// eslint-disable-next-line max-len
+const str_ = i18n.createIcuMessageFn('node_modules/@paulirish/trace_engine/models/trace/insights/UseCache.js', UIStrings);
+
+class UseCacheInsight extends Audit {
+  /**
+   * @return {LH.Audit.Meta}
+   */
+  static get meta() {
+    return {
+      id: 'use-cache-insight',
+      title: str_(UIStrings.title),
+      failureTitle: str_(UIStrings.title),
+      description: str_(UIStrings.description),
+      guidanceLevel: 3, // TODO: confirm/change.
+      requiredArtifacts: ['traces', 'TraceElements'],
+    };
+  }
+
+  /**
+   * @param {LH.Artifacts} artifacts
+   * @param {LH.Audit.Context} context
+   * @return {Promise<LH.Audit.Product>}
+   */
+  static async audit(artifacts, context) {
+    // TODO: implement.
+    return adaptInsightToAuditProduct(artifacts, context, 'UseCache', (insight) => {
+      /** @type {LH.Audit.Details.Table['headings']} */
+      const headings = [
+        /* eslint-disable max-len */
+        /* eslint-enable max-len */
+      ];
+      /** @type {LH.Audit.Details.Table['items']} */
+      const items = [
+      ];
+      return Audit.makeTableDetails(headings, items);
+    });
+  }
+}
+
+export default UseCacheInsight;
