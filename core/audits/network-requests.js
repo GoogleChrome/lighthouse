275aa369045d1a972cab861412b6405f28ed0f19
Boluwatife Ifeoluwa Omosowon <2816096o@student.gla.ac.uk>
2025-07-17 10:38:57 +0100

    added network initiator

--- core/audits/network-requests.js
+++ core/audits/network-requests.js
@@ -87,6 +87,7 @@ class NetworkRequests extends Audit {
         lrTCPMs: TCPMs, // Only exists on Lightrider runs
         lrRequestMs: requestMs, // Only exists on Lightrider runs
         lrResponseMs: responseMs, // Only exists on Lightrider runs
+        initiator: record.initiator,
       };
     });
 
