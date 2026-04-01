803cba8b3976b99e14ac6fb4e0f3ecbf69f01d53
Lusa Zhan <lusazhan@chromium.org>
2026-04-01 14:54:44 +0000

    include baseline date in description

--- core/audits/baseline.js
+++ core/audits/baseline.js
@@ -13,18 +13,17 @@ import * as i18n from '../lib/i18n/i18n.js';
 const UIStrings = {
   /** Title of the Baseline audit. Shown when the page is compatible with the target baseline. */
   title: 'Baseline Features',
-  /** Description of the Baseline audit. */
+  /**
+   * @description Description of the Baseline audit.
+   * @example {2026-03-18} date
+   */
   description:
-    'Lists web features used on the page and their Baseline status. ' +
+    'Lists web features used on the page and their Baseline status as of {date}. ' +
     '[Learn more about Baseline](https://webstatus.dev/).',
   /** Label for the column displaying the feature ID. */
   columnFeature: 'Web-features',
-  /**
-   * @description Label for the column displaying the feature\'s baseline status.
-   * @example {3.21.0} version
-   * @example {2026-03-30} date
-  */
-  columnStatus: 'Baseline Status (web-features {version} {date})',
+  /** Label for the column displaying the feature\'s baseline status. */
+  columnStatus: 'Baseline Status',
 };
 
 const str_ = i18n.createIcuMessageFn(import.meta.url, UIStrings);
@@ -42,7 +41,7 @@ class Baseline extends Audit {
       id: 'baseline',
       scoreDisplayMode: Audit.SCORING_MODES.INFORMATIVE,
       title: str_(UIStrings.title),
-      description: str_(UIStrings.description),
+      description: str_(UIStrings.description, {date: metadata.date}),
       requiredArtifacts: ['Trace'],
     };
   }
@@ -126,7 +125,7 @@ class Baseline extends Audit {
       {
         key: 'displayStatus',
         valueType: 'text',
-        label: str_(UIStrings.columnStatus, {version: metadata.version, date: metadata.date}),
+        label: str_(UIStrings.columnStatus),
       },
       {
         key: 'source',
