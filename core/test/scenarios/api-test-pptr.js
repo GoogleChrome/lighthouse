e5168faf4dc3be4f2408279241712b384a821bd7
Connor Clark <cjamcl@google.com>
2026-06-26 11:58:42 -0400

    oops

--- core/test/scenarios/api-test-pptr.js
+++ core/test/scenarios/api-test-pptr.js
@@ -163,11 +163,11 @@ describe('Individual modes API', function() {
       const {auditResults, erroredAudits, notApplicableAudits} = getAuditsBreakdown(result.lhr);
       expect(auditResults.map(audit => audit.id).sort()).toMatchSnapshot();
 
-      expect(notApplicableAudits.map(audit => audit.id).sort()).toMatchSnapshot();
       expect(notApplicableAudits
         .filter(audit => audit.id !== 'interaction-to-next-paint')
         .map(audit => audit.id)
-      ).not.toContain('total-blocking-time');
+      ).toMatchSnapshot();
+      expect(notApplicableAudits.map(audit => audit.id)).not.toContain('total-blocking-time');
 
       expect(erroredAudits).toHaveLength(0);
     });
