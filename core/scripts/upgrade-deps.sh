9dc56e2577b13212f3619bcbb4220b57688c5111
Connor Clark <cjamcl@google.com>
2026-08-31 12:40:51 -0700

    axe

--- core/scripts/upgrade-deps.sh
+++ core/scripts/upgrade-deps.sh
@@ -46,6 +46,20 @@ node -e "
       const metadataPath = '$LH_ROOT/core/lib/baseline/web-features-metadata.json';
       fs.writeFileSync(metadataPath, JSON.stringify({date}, null, 2) + '\n');
     }
+
+    // Update axe-core rule links in accessibility audits to match the installed version.
+    const axePkg = require('$LH_ROOT/node_modules/axe-core/package.json');
+    const [axeVer] = /^\d+\.\d+/.exec(axePkg.version);
+    const accessibilityDir = '$LH_ROOT/core/audits/accessibility';
+    for (const file of fs.readdirSync(accessibilityDir)) {
+      if (!file.endsWith('.js')) continue;
+      const filePath = accessibilityDir + '/' + file;
+      let content = fs.readFileSync(filePath, 'utf8');
+      if (content.includes('dequeuniversity.com/rules/axe/')) {
+        content = content.replace(/dequeuniversity\.com\/rules\/axe\/\d+\.\d+/g, 'dequeuniversity.com/rules/axe/' + axeVer);
+        fs.writeFileSync(filePath, content, 'utf8');
+      }
+    }
 "
 
 # Do some stuff that may update checked-in files.
